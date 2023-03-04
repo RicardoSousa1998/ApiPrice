19c3a60a7aa2ca37241731cabc89d59895d8824d
Ricardo Sousa <60556884+RicardoSousa1998@users.noreply.github.com>
2023-03-04 22:46:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, jsonify
 import time
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -5,81 +6,42 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-nametosearch = input("Pesquisa?")
+app = Flask(__name__)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+@app.route('/search', methods=['GET'])
+def search():
+    nametosearch = request.args.get('query')
 
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+    # Auchan Data
 
-# Continente Data
-contURLStart ='https://www.continente.pt/pesquisa/?q='
-contURLEnd = '&search-button=&lang=null'
-continenteURL = contURLStart+ nametosearch + contURLEnd
-driver.get(continenteURL)
-continenteResults = []
-continenteContent = driver.page_source
-continenteSoup = BeautifulSoup(continenteContent)
-continenteProducts = continenteSoup.findAll('div', attrs={"class":"productTile"})
+    auchanURLStart = 'https://www.auchan.pt/pt/pesquisa?q='
+    auchanURLEnd = '&search-button=&lang=null'
+    auchanURL = auchanURLStart + nametosearch + auchanURLEnd
+    driver.get(auchanURL)
+    auchanResults = []
+    auchanContent = driver.page_source
+    auchanSoup = BeautifulSoup(auchanContent)
+    auchanProducts = auchanSoup.findAll('div', attrs={"class":"auc-product"})
 
-for product in continenteProducts:
-    productName = product.find('a', attrs={"class":"ct-tile--description"}).text
-    productPrice = product.find('span', attrs={"class":"value"}).text.replace('\n','').replace('/un','').replace('€','').replace(',','.').replace('/Kg','').replace('\t','')
-    productDiscount = product.find('span', attrs={"class":"ct-product-tile-badge-value--pvpr"})
-    if productDiscount == None:
-        productDiscount = float('0')
-        #continue
-    else:
-        productDiscount = float(productDiscount.text.replace('\n',''))
-    continenteResults.append({"name":productName,"price":productPrice,"discount":productDiscount,"source":"Continente"})
-# Auchan Data
+    for product in auchanProducts:
+        productName = product.find('a', attrs={"class":"link"}).text
+        productPrice = product.find('span', attrs={"class":"sales"}).find('span', attrs={"class":"value"}).text.replace('\n','').replace('/un','').replace('€','').replace(',','.').replace('/Kg','').replace('\t','')
+        productDiscount = product.find('div', attrs={"class":"auc-promo--discount auc-promo--discount--red"})
+        if productDiscount == None:
+            productDiscount = float('0')
+        else:
+            productDiscount = float(productDiscount.text.replace('\n','').replace('-','').replace('%',''))
+        auchanResults.append({"name":productName,"price":productPrice,"discount":productDiscount,"source":"Auchan"})
 
-auchanURLStart = 'https://www.auchan.pt/pt/pesquisa?q='
-auchanURLEnd = '&search-button=&lang=null'
-auchanURL = auchanURLStart + nametosearch + auchanURLEnd
-driver.get(auchanURL)
-auchanResults = []
-auchanContent = driver.page_source
-auchanSoup = BeautifulSoup(auchanContent)
-auchanProducts = auchanSoup.findAll('div', attrs={"class":"auc-product"})
+    df = pd.DataFrame(auchanResults)
+    df = df.sort_values(by=['discount'], ascending=False)
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
 
-for product in auchanProducts:
-    productName = product.find('a', attrs={"class":"link"}).text
-    productPrice = product.find('span', attrs={"class":"sales"}).find('span', attrs={"class":"value"}).text.replace('\n','').replace('/un','').replace('€','').replace(',','.').replace('/Kg','').replace('\t','')
-    productDiscount = product.find('div', attrs={"class":"auc-promo--discount auc-promo--discount--red"})
-    if productDiscount == None:
-        productDiscount = float('0')
-        #continue
-    else:
-        productDiscount = float(productDiscount.text.replace('\n','').replace('-','').replace('%',''))
-    auchanResults.append({"name":productName,"price":productPrice,"discount":productDiscount,"source":"Auchan"})
-# Minipreco Data
+    return jsonify(df.to_dict('records'))
 
-miniprecoURLStart = 'https://www.minipreco.pt/search?text='
-miniprecoURLEnd = '&x=0&y=0'
-
-miniprecoURL = miniprecoURLStart + nametosearch + miniprecoURLEnd
-
-driver.get(miniprecoURL)
-miniprecoResults = []
-miniprecoContent = driver.page_source
-miniprecoSoup = BeautifulSoup(miniprecoContent)
-miniprecoProducts = miniprecoSoup.findAll('div', attrs={"class":"product-list__item"})
-
-for product in miniprecoProducts:
-    productName = product.find('span', attrs={"class":"details"}).text.replace('\n','').replace('\t','')
-    productPrice = product.find('p', attrs={"class":"price"}).text.replace('\n','').replace('/un','').replace('€','').replace(',','.').replace('/Kg','').replace('\t','')
-    productDiscount = product.find('span', attrs={"class":"label_text"})
-    if productDiscount == None:
-        productDiscount = float('0')
-        #continue
-    else:
-        productDiscount = float('1')
-    miniprecoResults.append({"name":productName,"price":productPrice,"discount":productDiscount,"source":"MiniPreco"})
-
-    allResults = continenteResults + auchanResults + miniprecoResults
-df = pd.DataFrame(continenteResults)
-df = df.sort_values(by=['discount'], ascending=False)
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-print(df)
+if __name__ == '__main__':
+    app.run(debug=True)
